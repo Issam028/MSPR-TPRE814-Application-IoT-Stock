757fce17e2ec383bb41792c7e2422c8544f06bbe
Tibow-DEV <thibaultautexier85@gmail.com>
2026-04-16 11:14:17 +0200

    Création de la branche

--- central/app/src/App.tsx
+++ central/app/src/App.tsx
@@ -4,12 +4,6 @@ import { TestsModal } from './components/Modal/Tests/TestsModal'
 import testIcon from './assets/icon/tests.png'
 import './App.css'
 
-async function getLots() {
-  const response = await fetch('http://localhost:3001/lots')
-  const data = await response.json()
-  console.log(data)
-}
-
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false)
 
