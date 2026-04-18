5851ccf2ced273b1d7c0ad9c0a006f3305d22651
Tibow-DEV <thibaultautexier85@gmail.com>
2026-04-18 11:05:27 +0200

    Page exploitation : Design de la température et de l'humidité

---
+++ docs/data_tests/entrepots.sql
@@ -0,0 +1,8 @@
+-- Avant l'utilisation de ce script, il faut 2 exploitation avec les ids de 1 - 2
+INSERT INTO `entrepots` (`id_entrepot`, `id_exploitation`, `nom`) VALUES (1, 1, 'ENT01');
+INSERT INTO `entrepots` (`id_entrepot`, `id_exploitation`, `nom`) VALUES (2, 1, 'ENT02');
+INSERT INTO `entrepots` (`id_entrepot`, `id_exploitation`, `nom`) VALUES (3, 1, 'ENT03');
+INSERT INTO `entrepots` (`id_entrepot`, `id_exploitation`, `nom`) VALUES (4, 1, 'ENT04');
+INSERT INTO `entrepots` (`id_entrepot`, `id_exploitation`, `nom`) VALUES (6, 2, 'ENT01');
+INSERT INTO `entrepots` (`id_entrepot`, `id_exploitation`, `nom`) VALUES (7, 2, 'ENT02');
+INSERT INTO `entrepots` (`id_entrepot`, `id_exploitation`, `nom`) VALUES (8, 2, 'ENT03');
